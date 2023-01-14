b8652bf767760294d7f7677b699f594db5566bcb
ZetaCo18 <brandonstevenvera+1@gmail.com>
2023-01-14 14:19:14 -0300

    guardando proceso avance javascript

--- js/javascript.js
+++ js/javascript.js
@@ -4,9 +4,20 @@ function encriptarTexto(){
     document.getElementById("texto2").value=texto1;
     // oculta el elemento 
     document.getElementById('ocultarId').style.display = 'none';
+    //mostrar textArea Derecho
+    document.getElementById('texto2').style.display = 'block';
+    //mostar boton
+    document.getElementById('btnMostrar').style.display = 'block';
 }
 function desencriptarTexto(){
     // manda el texto al box # 1 
     let texto2 = document.getElementById('texto2').value;
     document.getElementById("texto1").value=texto2;
+}
+
+function copiar(){
+    let texto = document.getElementById('texto2');
+    texto.select();
+    texto.setSelectionRange(0,99999);
+    document.execCommand('copy')
 }
