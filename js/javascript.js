ca6ae6c935cacb0a2a409e133401fbb3a51ccc1e
ZetaCo18 <brandonstevenvera+1@gmail.com>
2023-01-14 15:29:03 -0300

    me explota la cabeza el js

--- js/javascript.js
+++ js/javascript.js
@@ -15,9 +15,28 @@ function desencriptarTexto(){
     document.getElementById("texto1").value=texto2;
 }
 
+// FUNCION COPIAR 
 function copiar(){
     let texto = document.getElementById('texto2');
     texto.select();
     texto.setSelectionRange(0,99999);
     document.execCommand('copy')
+}
+
+// FUNCION REMPLAZAR
+
+let cadena = ['a','e','i','o','u'];
+let cadena2 = ['ai','enter','imes','ober', 'ufat'];
+
+function remplazarTexto(){
+    document.getElementById('texto1').value;
+}
+
+
+// RESTRINGIR CARACTERES
+
+function restringirCaracteres(){
+    let caracteres =  (document.getElementById('texto1').value){
+        
+    }
 }
